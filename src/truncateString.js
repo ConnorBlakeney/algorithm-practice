3b379263a4e238b4a03f9247cc03a3cc341a5654
Connor Blakeney <connorblakeney@yahoo.com>
2020-08-24 08:42:10 -0500

    solved truncate string problem

--- src/truncateString.js
+++ src/truncateString.js
@@ -2,10 +2,21 @@
 // given maximum string length (second argument). 
 // Return the truncated string with a ... ending.
 
+// const truncateString = (str, num) => {
+//   if (num < str.length) {
+//     return str.slice(0, num) + "..."
+//   } else {
+//         return str
+//     }
+// }
+
 const truncateString = (str, num) => {
-  if (num < str.length) {
-    return str + "...";
-  }
+    return str.length > num ? str.slice(0, num) + "..." : str
 }
 
-truncateString("A-tisket a-tasket A green and yellow basket", 8);
+console.log(truncateString("A-tisket a-tasket A green and yellow basket", 8))
+console.log(truncateString("Peter Piper picked a peck of pickled peppers", 11))
+console.log(truncateString("A-tisket a-tasket A green and yellow basket", "A-tisket a-tasket A green and yellow basket".length))
+console.log(truncateString("A-tisket a-tasket A green and yellow basket", "A-tisket a-tasket A green and yellow basket".length + 2))
+console.log(truncateString("A-", 1))
+console.log(truncateString("Absolutely Longer", 2))
